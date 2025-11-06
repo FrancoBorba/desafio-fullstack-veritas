438daefb5fa310dad18879ad53515b07235cbefb
Franco Borba <franco.borba14@gmail.com>
2025-11-06 17:52:25 -0300

    Feat(Backend) improves task  model

--- backend/models.go
+++ backend/models.go
@@ -4,12 +4,16 @@ type Task struct {
 	ID          string `json:"id"`
 	Title       string `json:"title" validate:"required,min=3"` // The title is required and must be at least 3 characters long.
 	Description string `json:"description,omitempty"`          // omitempty = If it's empty, it won't appear in the JSON.
-	Status      string `json:"status" validate:"required,oneof='A Fazer' 'Em Progresso' 'Concluídas'"`
+	Prioridade  string `json:"prioridade,omitempty"`
+	Status      string `json:"prioridade" validate:"omitempty,oneof='Alta' 'Média' 'Baixa'"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
 
 // UpdateTaskPayload is the DTO we use for the UPDATE (PUT/PATCH) route.
 type UpdateTaskPayload struct {
 	Title       *string `json:"title,omitempty" validate:"omitempty,min=3"`
 	Description *string `json:"description,omitempty"`
+	Prioridade  *string `json:"prioridade,omitempty"`
 	Status      *string `json:"status,omitempty" validate:"omitempty,oneof='A Fazer' 'Em Progresso' 'Concluídas'"`
-}
+}	
